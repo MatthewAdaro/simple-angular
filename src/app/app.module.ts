60f99e662b8820b2c0ce46fc0bfa2682f415bb0b
Matthew Adaro <117700776+MatthewAdaro@users.noreply.github.com>
2023-05-09 16:19:22 +0800

    Add a fake Store API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
@@ -9,7 +9,12 @@ import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass'],
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  data$: Observable<any[]> = new Observable<any[]>();
 
-  back() {
+  constructor(private router: Router, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  back(): void {
     this.router.navigate(['/']);
+  }
+
+  fetchData() {
+    this.data$ = this.http.get<any[]>('https://fakestoreapi.com/products');
   }
 }
